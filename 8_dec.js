c9abccafa88d817d00a82b62f680e411f3f5bb5d
akshay <akshayap4141@gmail.com>
2023-12-08 18:47:14 +0530

    data types summary

---
+++ 8_dec.js
@@ -0,0 +1,36 @@
+//comaprisons of data types
+
+// console.log("2" > 1);
+// console.log("02" < 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+// console.log(undefined <= 0);
+
+// const id = Symbol('123');
+// const anotherId =Symbol('123');
+// console.log(typeof anotherId);
+// console.log(id);
+// console.log(anotherId);
+
+// const bigNumber = 328383787383823827827n
+// console.log(bigNumber);
+
+// const myObj = {
+//     name: "Akshay",
+//     age: 28,
+// }
+
+// console.log(myObj);
+
+
+
+
+
+
+
