c6646776db4336c4f86d03a50fca7fd228fadf5f
akshay <akshayap4141@gmail.com>
2023-12-08 09:36:35 +0530

    updated 7 dec file

---
+++ 7_dec.js
@@ -0,0 +1,12 @@
+
+// it is decided by ECMA stds which type has priority for conversion
+// console.log("1" + 2 + 3);  //123
+// console.log(1 + 2 + "3");  //33
+
+// console.log(true);   //true
+// console.log(+true); //1
+// console.log(+"");   //0
+
+// for(let i=0;i<10;i++){
+//     console.log(i);
+// }
