0c6432f1bed96c484728e6169677687d75eb5f85
akshay <akshayap4141@gmail.com>
2023-12-09 13:18:58 +0530

    string number array

---
+++ 9_dec.js
@@ -0,0 +1,107 @@
+//const name1 = new String('Akshay');
+
+// console.log(name1.toUpperCase());
+
+// console.log(name1.charAt(2));
+
+// console.log(name1.indexOf('k'));
+
+// const newString = name1.substring(0,4);
+
+// console.log(newString);
+
+// const anotherString = name1.slice(2,5);
+
+// console.log(anotherString);
+
+//const str = "   Sunny  "
+// console.log(str);
+// console.log(str.trim());
+
+ //const str1 = "Gaurav";
+// console.log(str1.replace("v","z"));
+
+//console.log(str1.includes("u"));
+
+// const str3 = "Ashok-patil-Narayan";
+// console.log(str3.split("-")); //[ 'Ashok', 'patil', 'Narayan' ]
+
+//Number and maths
+
+// const num = new Number(100);
+
+// console.log(num.toString().length);
+
+// console.log(num.toFixed(2));
+
+// const pre = 23.456;
+// console.log(pre.toPrecision(3)); //23.5
+
+// const hundreds = 10000000;
+// console.log(hundreds.toLocaleString());
+
+//++++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));  //4
+// console.log(Math.round(5.78)); //6
+// console.log(Math.ceil(4.2)); //5
+// console.log(Math.floor(4.9));  //4
+
+//Math.min and Math.min
+
+//console.log(Math.floor((Math.random() * 10) + 1));
+
+// const min = 10;
+// const max = 20;
+
+// console.log(Math.floor(Math.random() * (max- min + 1) + min));
+
+//+++++++++++++++++++++++++ Date and Time +++++++++++++++++++++++++++++++++++++++++
+
+// const myDate = new Date();
+
+// console.log(myDate); //2023-12-09T05:56:27.662Z
+// console.log(myDate.toString()); //Sat Dec 09 2023 11:26:27 GMT+0530 (India Standard Time)
+// console.log(myDate.toLocaleString());  //9/12/2023, 11:26:27 am
+
+// console.log(typeof myDate); //object
+
+//const newDate = Date.now();
+
+// console.log(newDate);
+// console.log(myDate.getTime());
+
+// console.log(Math.floor(Date.now() / 1000));
+
+//++++++++++++++++++++++++++ Arrays +++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+const myArray = [1,4,2,5,1,4,6,8];
+
+// myArray.sort();
+// console.log(myArray);
+
+//Array Methods
+
+// myArray.push(6);
+// myArray.pop(); //pop the peak value
+
+// myArray.unshift(9);
+// myArray.shift();
+// console.log(myArray);
+
+// console.log(myArray.includes(5));
+// console.log(myArray.indexOf(8));
+
+// const newArr = myArray.join();  //join converts array into string
+// console.log(newArr);
+// console.log(typeof newArr); //string
+
+//slice - do not include range and manipulate array
+//spice - include range and manipulate original array
+
+// console.log(myArray.slice(1,3));
+// console.log(myArray.splice(1,3));
+
+
+
