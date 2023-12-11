58ff793d432bab9c972f4631c9c41dcfdffb8b64
akshay <akshayap4141@gmail.com>
2023-12-11 09:31:06 +0530

    array and objects

---
+++ 10_dec.js
@@ -0,0 +1,21 @@
+// const tinderUser = new Object({name: "Akshay",age: 28});
+
+// //tinderUser.name="Akshay";
+
+// console.log(tinderUser);
+
+// const obj = {1: "a"};
+
+// const obj2 = {2:"b"};
+
+// const obj3 = {...obj,...obj2};
+
+// console.log(obj3);
+
+//++++++++++++++++ functions ++++++++++++++++++++++++++
+
+function addTwo(a,b) {
+        return a+b;
+}
+
+console.log(addTwo(2,3));
